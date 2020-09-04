166228304803a8f425e3212f7e8efb1a9805ab94
Konstantin <konzi.h@gmail.com>
2020-09-04 12:47:17 +0200

    Brighter photos

--- data.js
+++ data.js
@@ -1,145 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-wohnzimmer",
-      "name": "Wohnzimmer",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.673634143294148,
-          "pitch": 0.29317683757684243,
-          "rotation": 1.5707963267948966,
-          "target": "1-schlafzimmer"
-        },
-        {
-          "yaw": 0.6752166807174618,
-          "pitch": 0.14872128849357047,
-          "rotation": 0.7853981633974483,
-          "target": "2-bad"
-        },
-        {
-          "yaw": 0.6913457453585163,
-          "pitch": 0.010842291492487277,
-          "rotation": 0,
-          "target": "3-kche"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-schlafzimmer",
-      "name": "Schlafzimmer",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.1089008666716822,
-          "pitch": 0.31278965354627175,
-          "rotation": 4.71238898038469,
-          "target": "0-wohnzimmer"
-        },
-        {
-          "yaw": 0.2606024047317561,
-          "pitch": 0.018034357436087944,
-          "rotation": 4.71238898038469,
-          "target": "3-kche"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-bad",
-      "name": "Bad",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.09170230809641211,
-          "pitch": 0.24236665367410026,
-          "rotation": 0,
-          "target": "0-wohnzimmer"
-        },
-        {
-          "yaw": -0.26212275699096566,
-          "pitch": 0.3565526013335578,
-          "rotation": 4.71238898038469,
-          "target": "1-schlafzimmer"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-kche",
+      "id": "0-kche",
       "name": "Küche",
       "levels": [
         {
@@ -168,22 +30,166 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.11575234471131957,
-          "pitch": 0.32028830411336884,
+          "yaw": 0.0857541567682496,
+          "pitch": 0.17530455738531003,
           "rotation": 1.5707963267948966,
-          "target": "0-wohnzimmer"
+          "target": "1-wohnzimmer"
         },
         {
-          "yaw": 0.10939360502245421,
-          "pitch": 0.19531814169557293,
+          "yaw": 0.09235236284238724,
+          "pitch": 0.2626355863586056,
           "rotation": 1.5707963267948966,
-          "target": "1-schlafzimmer"
+          "target": "2-schlafzimmer"
         },
         {
-          "yaw": 0.10049564119690402,
-          "pitch": 0.06612718906286297,
-          "rotation": 7.853981633974483,
-          "target": "2-bad"
+          "yaw": 0.09261045022311265,
+          "pitch": 0.34700605725241296,
+          "rotation": 1.5707963267948966,
+          "target": "3-bad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wohnzimmer",
+      "name": "Wohnzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6774003977874639,
+          "pitch": 0.29374024112345154,
+          "rotation": 0.7853981633974483,
+          "target": "3-bad"
+        },
+        {
+          "yaw": 0.7169524537440513,
+          "pitch": 0.15395094273461396,
+          "rotation": 1.5707963267948966,
+          "target": "2-schlafzimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-schlafzimmer",
+      "name": "Schlafzimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26948430637459,
+          "pitch": 0.1893559583028086,
+          "rotation": 5.497787143782138,
+          "target": "0-kche"
+        },
+        {
+          "yaw": 0.13022904913398037,
+          "pitch": 0.3478144583753071,
+          "rotation": 4.71238898038469,
+          "target": "1-wohnzimmer"
+        },
+        {
+          "yaw": 0.4677877315798469,
+          "pitch": 0.3883486261918492,
+          "rotation": 1.5707963267948966,
+          "target": "3-bad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bad",
+      "name": "Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0729254258340859,
+          "pitch": 0.33627652262191177,
+          "rotation": 0,
+          "target": "1-wohnzimmer"
+        },
+        {
+          "yaw": -0.2147928841380562,
+          "pitch": 0.34265642408482755,
+          "rotation": 4.71238898038469,
+          "target": "2-schlafzimmer"
+        },
+        {
+          "yaw": 0.20094682019380627,
+          "pitch": 0.07907031399781062,
+          "rotation": 1.5707963267948966,
+          "target": "0-kche"
         }
       ],
       "infoHotspots": []
